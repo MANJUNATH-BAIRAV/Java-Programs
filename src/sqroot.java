4ab23e050182e83797c785f479ff7f35f33ba2b3
MANJUNATH-BAIRAV <manjunathbairav1@gmail.com>
2025-02-27 09:31:51 +0530

    Update sqroot.java

--- src/sqroot.java
+++ src/sqroot.java
@@ -1,13 +1,11 @@
-import java.lang.Math;
-class sqroot
- {
-  public static void main(String args[])
-   { 
-     double x=5;
-     double y;
-     y = Math.sqrt(x);
-     System.out.println("y="+y);
-   }
- }
+package com.manjunath;
 
-     
+import java.lang.Math;
+
+public class sqroot {
+  public static void main(String args[]) { 
+     double x = 5;
+     double y = Math.sqrt(x);
+     System.out.println("y=" + y);
+  }
+}
